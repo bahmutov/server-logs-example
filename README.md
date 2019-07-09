1c56e52b782688fef23eb290f3dc6f9eaa555823
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-08 21:37:21 -0400

    initial server

---
+++ README.md
@@ -0,0 +1,2 @@
+# server-logs-example
+> API testing using Cypress.io test runner with server-side logs
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const url = require('url')
+const querystring = require('querystring')
+
+http
+  .createServer(function (req, res) {
+    const { pathname, query } = url.parse(req.url, true)
+
+    console.log(req.url)
+    console.log('pathname', pathname)
+    console.log('query', query)
+
+    res.writeHead(200)
+    res.write(String(parseFloat(query.a) + parseFloat(query.b)))
+    res.end()
+  })
+  .listen(7000, () => {
+    console.log('listening on port 7000')
+  })
